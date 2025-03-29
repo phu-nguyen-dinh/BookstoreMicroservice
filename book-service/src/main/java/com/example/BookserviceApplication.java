c462c3c29ae0c82c85ca932d86cee5d47b7bb02c
Nguyen Dinh Phu <phu0981838248@gmail.com>
2025-03-29 17:47:58 +0700

    Initial commit - Microservice BookStore

---
+++ book-service/src/main/java/com/example/BookserviceApplication.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class BookserviceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookserviceApplication.class, args);
+    }
+
+    // 👇 Controller test API
+    @RestController
+    public static class HelloController {
+
+        @GetMapping("/books/hello")
+        public String hello() {
+            return "📚 Hello from Book Service!";
+        }
+    }
+}
---
+++ discovery-server/src/main/java/com/example/DiscoveryserverApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryserverApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryserverApplication.class, args);
+    }
+}
