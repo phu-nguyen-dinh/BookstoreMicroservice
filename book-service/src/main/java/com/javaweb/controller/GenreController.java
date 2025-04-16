465c9c1bba4ad0b9f0eec96e402d50631dc5b513
Nguyen Dinh Phu <phu0981838248@gmail.com>
2025-04-16 19:26:57 +0700

    add Controller for Author, Book and Genre

---
+++ book-service/src/main/java/com/javaweb/controller/GenreController.java
@@ -0,0 +1,9 @@
+package com.javaweb.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/genres")
+public class GenreController {
+
+}
